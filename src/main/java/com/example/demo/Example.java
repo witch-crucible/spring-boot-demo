8cb5c8901989e27b234b602648b68220f7787ab5
benhuang <benhuang1024@gmail.com>
2020-02-16 01:30:40 +0800

    todo

---
+++ src/main/java/com/example/demo/Example.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+public class Example {
+
+}
